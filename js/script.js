b76a14300e40346f6ce6c1e13f472c09a378f6c1
Lais <sanolilais@gmail.com>
2022-10-11 22:16:52 -0300

    add - message game over

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 const mario = document.querySelector('.image__mario')
-
+const tunnel = document.querySelector('.image__tunnel')
+const msgGameOver =  document.getElementById('#gameover')
 
 const jump = () => {
     mario.classList.add('jump--animation');
 
     setTimeout(() => {
         mario.classList.remove('jump--animation');
-    }, 500)
+    }, 1500)
 }
+const loop = setInterval(() => {
+    const tunnelPosition = tunnel.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px' , '')
 
-document.addEventListener('keydown' , jump)
+    if (tunnelPosition <= 120 && tunnelPosition > 0  && marioPosition < 100 ) {
+        tunnel.style.animation = 'none'
+        tunnel.style.left = `${tunnelPosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './assets/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        msgGameOver.innerHTML = 'GAME OVER'
+
+        clearInterval(loop)
+    
+    }
+} , 10)
+
+document.addEventListener('keypress' , jump) 
